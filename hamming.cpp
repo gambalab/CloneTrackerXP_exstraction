d4243114b0f8c9921deadb7c9185ebe7fc435988
Gennaro Gambardella <66130080+gambalab@users.noreply.github.com>
2024-07-31 11:36:49 +0200

    remove duplication

--- hamming.cpp
+++ hamming.cpp
@@ -13,29 +13,6 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 // Hamming Dist between string of the same length and where the Ns in the reference do not count
-arma::mat hammingDist(const arma::mat& m, const arma::mat& m2, int ncores=1,bool verbose=true,int th=6)
-{
-  arma::mat hdist(m.n_rows, m2.n_rows);
-  hdist.fill(0);
-  Progress p(m.n_rows*m2.n_rows, verbose);
-  #pragma omp parallel for num_threads(ncores) shared(hdist)
-  for(int i=0;i<m.n_rows;i++) {
-    for(int j=0;j<m2.n_rows;j++)
-    {
-      int mismatchs = 0;
-      for(int k=0;k<std::min(m.n_cols,m2.n_cols) && mismatchs<=th;k++) {
-        if( (m2(j,k) != 0) && (m(i,k) != m2(j,k)) ) {
-          mismatchs++;
-        }
-      }
-      hdist(i,j) = mismatchs;
-    }
-  }
-  return hdist;
-}
-
-// [[Rcpp::export]]
-// Hamming Dist between string of the same length and where the Ns in the reference do not count
 arma::mat hammingDistAll(const arma::mat& m, const arma::mat& m2, int ncores=1,bool verbose=true,int th=6)
 {
   arma::mat hdist(m.n_rows, m2.n_rows);
